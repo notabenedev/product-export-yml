89dfd3d23dd154f4317ca99a52da5d0280b13bbf
GIS <dev@gis4biz.ru>
2023-11-29 10:07:28 +0300

    initial commit

---
+++ src/ProductExportYmlServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Notabenedev\ProductExportYml;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProductExportYmlServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+}
